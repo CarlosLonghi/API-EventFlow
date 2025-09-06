e2d21129556c742041f1b8b1f651cbc637af725b
carlos <carloslonghi.cl@gmail.com>
2025-09-06 00:39:26 -0300

    feat: implement persistence gateway with EventRepository

---
+++ src/main/java/br/com/carloslonghi/eventflow/api/infra/gateway/EventRepositoryGateway.java
@@ -0,0 +1,26 @@
+package br.com.carloslonghi.eventflow.api.infra.gateway;
+
+import br.com.carloslonghi.eventflow.api.core.entities.Event;
+import br.com.carloslonghi.eventflow.api.core.gateway.EventGateway;
+import br.com.carloslonghi.eventflow.api.infra.mapper.EventEntityMapper;
+import br.com.carloslonghi.eventflow.api.infra.persistence.EventEntity;
+import br.com.carloslonghi.eventflow.api.infra.persistence.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EventRepositoryGateway implements EventGateway {
+
+    private final EventRepository eventRepository;
+    private final EventEntityMapper eventEntityMapper;
+
+    @Override
+    public Event createEvent(Event event) {
+        EventEntity eventEntity = eventEntityMapper.toEntity(event);
+
+        EventEntity eventSaved = eventRepository.save(eventEntity);
+
+        return eventEntityMapper.toDomain(eventSaved);
+    }
+}
